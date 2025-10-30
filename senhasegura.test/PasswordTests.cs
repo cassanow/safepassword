60b3d443bbd35e507c598ad7190ab6496ca9e70f
Arthur <nashcassano8@gmail.com>
2025-10-30 02:35:23 -0300

    feat: implementando mais testes unitarios para me retornar com sucesso senhas com seguranca baixa, media e alta

--- senhasegura.test/PasswordTests.cs
+++ senhasegura.test/PasswordTests.cs
@@ -1,5 +1,4 @@
-﻿using senhasegura.Interface;
-using senhasegura.Service;
+﻿using senhasegura.Service;
 
 namespace senhasegura.test
 {
@@ -20,6 +19,33 @@ namespace senhasegura.test
             Assert.Equal(10, resultado.Pontuacao);
             Assert.Equal(Enum.Seguranca.Alta, resultado.Seguranca);
             Assert.True(resultado.Requisitos.temMinuscula);
+        }
+
+        [Fact]
+        public void DeveMeRetornarAVerificacaoComSegurancaBaixa()
+        {
+                     
+            var resultado = _service.VerifyPassword("senha");
+            Assert.Equal(2, resultado.Pontuacao);
+            Assert.Equal(Enum.Seguranca.Baixa, resultado.Seguranca);
+        }
+
+        [Fact]
+        public void DeveMeRetornarAVerificacaoComSegurancaMedia()
+        {
+                     
+            var resultado = _service.VerifyPassword("Senha123");
+            Assert.Equal(6, resultado.Pontuacao);
+            Assert.Equal(Enum.Seguranca.Media, resultado.Seguranca);
+        }
+
+        [Fact]
+        public void DeveMeRetornarAVerificacaoComSegurancaAlta()
+        {
+                     
+            var resultado = _service.VerifyPassword("Senh@12345");
+            Assert.Equal(10, resultado.Pontuacao);
+            Assert.Equal(Enum.Seguranca.Alta, resultado.Seguranca);
         }
 
     }
