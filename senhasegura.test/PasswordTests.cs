551834b24ff5f23529be0b13b4d4c436269b4ac7
Arthur <nashcassano8@gmail.com>
2025-10-30 02:22:53 -0300

    feat: adicionando testes unitarios e verificando se o metodo verificar senha esta funcionando corretamente

---
+++ senhasegura.test/PasswordTests.cs
@@ -0,0 +1,19 @@
+﻿using senhasegura.Service;
+
+namespace senhasegura.test
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void DeveMeRetornarAVerificacaoDaSenhaComSucesso()
+        {
+            SenhaService service = new SenhaService();
+
+            var resultado = service.VerifyPassword("Senha@1234");
+
+            Assert.Equal(10, resultado.Pontuacao);
+            Assert.Equal(Enum.Seguranca.Alta, resultado.Seguranca);
+            Assert.True(resultado.Requisitos.temMinuscula);
+        }
+    }
+}
