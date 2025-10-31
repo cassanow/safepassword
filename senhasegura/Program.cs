ea8e92c4db6e290f784132ecb193ff33a394df37
Arthur <nashcassano8@gmail.com>
2025-10-31 14:34:37 -0300

    refactor: fazendo uma pequena mudanda no bloco de codigo do cors

--- senhasegura/Program.cs
+++ senhasegura/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(builder =>
 {
     builder.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500/")
+        policy.WithOrigins("http://127.0.0.1:5500")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
