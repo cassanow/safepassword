e29f8856244d921d7a4464380c3ecdcbd63504e1
Arthur <nashcassano8@gmail.com>
2025-10-31 14:30:31 -0300

    implementando o fetch pra consumir o backend quando eu apertar o botao

--- senhasegura.front/script.js
+++ senhasegura.front/script.js
@@ -5,5 +5,9 @@ document.getElementById("botao-verificar").addEventListener("click", async funct
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ password })
-    })
+    });
+    console.log(response)
+
+    const resultado = await response.json();
+    document.getElementById("resultado").textContent = resultado.mensagem
 })
